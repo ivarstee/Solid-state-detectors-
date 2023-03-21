ac5e1eb21fdb452405ba129fe8f07b62621224a0
ivarstee <128475302+ivarstee@users.noreply.github.com>
2023-03-21 12:45:45 +0100

    Update tld(3).py

--- tld(3).py
+++ tld(3).py
@@ -54,7 +54,7 @@ def fit(pure, x, y, sub1, sub2, file): #sub1 = avyCa, sub2 = avyCu
     return a1, a2, r2
 
 def index(N, Y, i, numb):
-    if N[i] > numb: #351 er gjennomsnitt av indeksen til originale topp punkter (i første måling), definerer derfor dette som indeks og bytter alle til dette.
+    if N[i] > numb: 
 
         p = N[i]-numb
         Y[i] = Y[i][int(p):]
